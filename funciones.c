2f87bac1c4dcc14a7eb93dded3c7cffa200d7558
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-04-30 00:45:48 -0300

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -40,18 +40,12 @@ int obtenerEspacioLibre(EPersona lista[], int arrayLenght){
 }
 
 /**
-
  * Obtiene el indice que coincide con el dni pasado por parametro.
-
  * \param lista El array se pasa como parametro.
  * \param arrayLenght Indica la longitud del array
-
  * \param dni El dni a ser buscado en el array.
-
  * \return Si no hay coincidencia (-1) y si la hay, el indice en donde se encuentra el elemento que coincide con el parametro dni
-
  */
-
 
 int buscarPorDni(EPersona lista[], int arrayLenght, int dni){
     int i;
@@ -64,11 +58,8 @@ int buscarPorDni(EPersona lista[], int arrayLenght, int dni){
 }
 
 /**
-
  * Muestra los datos almacenados en una estructura de persona.
-
  * \param persona El array que se va a mostrar.
-
  */
 
 void mostrarPersona(EPersona persona){
@@ -76,12 +67,9 @@ void mostrarPersona(EPersona persona){
 }
 
 /**
-
  * Da de alta una nueva persona.
-
  * \param array El array donde se va a almacenar.
  * \param arrayLenght La longitud del array donde se va a almacenar.
-
  */
 
 void altaPersona(EPersona array[], int arrayLenght){
@@ -153,10 +141,8 @@ void altaPersona(EPersona array[], int arrayLenght){
 /**
 
  * Borra de la lista, luego de confirmar la baja, a la persona con el DNI ingresado.
-
  * \param array El array donde se va a buscar la persona con el DNI ingresado.
  * \param arrayLenght La longitud del array donde se va a almacenar.
-
  */
 
 void borrarPersona(EPersona array[], int arrayLenght){
@@ -201,10 +187,8 @@ void borrarPersona(EPersona array[], int arrayLenght){
 /**
 
  * Ordena alfabeticamente el array ingresado.
-
  * \param array El array que se ordenara.
  * \param arrayLenght La longitud del array.
-
  */
 
 void listaOrdenada(EPersona array[], int arrayLenght){
@@ -240,10 +224,8 @@ void listaOrdenada(EPersona array[], int arrayLenght){
 /**
 
  * Imprime grafico de barras de 3 grupos de edades, menores de 18, entre 19 a 35 y mayores de 35.
-
  * \param array El array que se ordenara.
  * \param arrayLenght La longitud del array.
-
  */
 
 void imprimrGrafico(EPersona array[], int arrayLenght){
