728bbce1b9db8336266059e78ea865561151b054
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-05-06 16:25:50 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,29 +1,3 @@
-/******************************************************************
- * Programación I – Laboratorio I
- * Programa: Trabajo práctico No 2.
- *
- * Objetivo:
- * Definir una estructura que represente una persona, con los campos nombre, edad, dni y un flag de estado.
- *
- *	    Hacer un programa que defina un array de 20 ítems de esta estructura y con un menú como el siguiente:
- *      1. Agregarunapersona
- *      2. Borrarunapersona
- *      3. Imprimirlistaordenadapornombre
- *      4. Imprimirgráficodeedades
- *
- * Aspectos a destacar:
- *
- *   Se utilizará el campo de estado para indicar si el ítem del array esta ocupado o no.
- *   El gráfico deberá ser un gráfico de barras en donde se agruparán 3 barras:
- *      • Menores de 18
- *      • De19a35
- *      • Mayores de 35.
- *      En el eje Y se marcarán la cantidad de personas en el grupo, y en el eje X el grupo
- *
- * Version: 0.2 del 06 de mayo de 2018
- * Autor: Gaston Pesoa
- *
- * *******************************************************************/
 
 #ifndef __TP_2_PROGRAMACION_I__funciones__
 #define __TP_2_PROGRAMACION_I__funciones__
